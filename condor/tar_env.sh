90567a8c8c46e2b8d60d211a854c0cf5f29fd59e
Lucas Corcodilos <lcorcodi@cern.ch>
2021-04-20 17:09:58 -0500

    Make run_snapshot.sh, tar_env.sh, and dijet_nano/get_all.py generic to user

--- condor/tar_env.sh
+++ condor/tar_env.sh
@@ -1,4 +1,4 @@
 cd $CMSSW_BASE/../
-tar --exclude-caches-all --exclude-vcs --exclude-caches-all --exclude-vcs -cvzf THBoostedAllHad.tgz CMSSW_11_1_4 --exclude=tmp --exclude=".scram" --exclude=".SCRAM" --exclude=CMSSW_11_1_4/src/timber-env --exclude=CMSSW_11_1_4/src/TH*.root
-xrdcp -f THBoostedAllHad.tgz root://cmseos.fnal.gov//store/user/lcorcodi/THBoostedAllHad.tgz
+tar --exclude-caches-all --exclude-vcs --exclude-caches-all --exclude-vcs -cvzf THBoostedAllHad.tgz CMSSW_11_1_4 --exclude=tmp --exclude=".scram" --exclude=".SCRAM" --exclude=CMSSW_11_1_4/src/timber-env --exclude=CMSSW_11_1_4/src/topHBoostedAllHad/TH*.root --exclude=CMSSW_11_1_4/src/topHBoostedAllHad/logs --exclude=CMSSW_11_1_4/src/topHBoostedAllHad/plots
+xrdcp -f THBoostedAllHad.tgz root://cmseos.fnal.gov//store/user/$USER/THBoostedAllHad.tgz
 cd $CMSSW_BASE/src/topHBoostedAllHad/
