613e3f9bb52b725134f26fa9fb49c3a0b7a19f04
Lucas Corcodilos <lcorcodi@cern.ch>
2021-04-23 21:48:47 -0500

    Add initial THplotter

---
+++ THplotter.py
@@ -0,0 +1,43 @@
+from argparse import Namespace
+from glob import glob
+from THselection import main
+from TIMBER.Tools.Common import DictStructureCopy
+import multiprocessing
+
+def GetAllFiles():
+    return glob('dijet_nano/*_snapshot.txt')
+
+def GetProcYear(filename):
+    pieces = filename.split('/')[-1].split('.')[0].split('_')
+    return pieces[0], pieces[1]
+
+def GetFileDict(all_files):
+    by_year = {
+        16: {'bkg':[],'sig':[],'data':None},
+        17: {'bkg':[],'sig':[],'data':None},
+        18: {'bkg':[],'sig':[],'data':None}
+    }
+    for f in all_files:
+        proc, year = GetProcYear(f)
+        if 'Tprime' in proc:
+            by_year[year]['sig'].append(f)
+        elif proc == 'Data':
+            by_year[year]['data'] = f
+        else:
+            by_year[year]['bkg'].append(f)
+    return by_year
+
+def multicore():
+    files = GetAllFiles()
+    args = Namespace()
+    pool = multiprocessing.Pool(processes=4)
+    process_args = []
+    for f in files:
+        args.proc, args.year = GetProcYear(f)
+        args.threads = 4
+        process_args.append(args)
+    pool.map(main,process_args)
+
+def plot():
+    hists = DictStructureCopy(GetAllFiles())
+
