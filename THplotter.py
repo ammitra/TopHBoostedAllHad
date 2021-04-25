20bb0bbe53fca358dab5d906186ba82329c2c011
Lucas Corcodilos <lac352@scarletmail.rutgers.edu>
2021-04-25 17:51:09 -0400

    Multithread setup

--- THplotter.py
+++ THplotter.py
@@ -1,47 +1,99 @@
 from argparse import Namespace
 from glob import glob
 from THselection import main
-from TIMBER.Tools.Common import DictStructureCopy
-import multiprocessing
+from TIMBER.Tools.Common import DictStructureCopy, CompileCpp, ExecuteCmd, OpenJSON, StitchQCD
+from TIMBER.Tools.Plot import CompareShapes
+import multiprocessing, ROOT
 
 def GetAllFiles():
-    return glob('dijet_nano/*_snapshot.txt')
-
-def GetProcYear(filename):
+    return [f for f in glob('dijet_nano/*_snapshot.txt') if f != '']
+def GetProcYearFromTxt(filename):
     pieces = filename.split('/')[-1].split('.')[0].split('_')
     return pieces[0], pieces[1]
+def GetProcYearFromROOT(filename):
+    pieces = filename.split('/')[-1].split('.')[0].split('_')
+    return pieces[1], pieces[2]
 
-def GetFileDict(all_files):
-    by_year = {
-        16: {'bkg':[],'sig':[],'data':None},
-        17: {'bkg':[],'sig':[],'data':None},
-        18: {'bkg':[],'sig':[],'data':None}
+def GetHistDict(histname, all_files):
+    all_hists = {
+        'bkg':{},'sig':{},'data':None
     }
     for f in all_files:
-        proc, year = GetProcYear(f)
+        proc, year = GetProcYearFromROOT(f)
+        tfile = ROOT.TFile.Open(f)
+        hist = tfile.Get(histname)
+        hist.SetDirectory(0)
         if 'Tprime' in proc:
-            by_year[year]['sig'].append(f)
+            all_hists['sig'][proc] = hist
         elif proc == 'Data':
-            by_year[year]['data'] = f
+            all_hists['data'] = hist
         else:
-            by_year[year]['bkg'].append(f)
-    return by_year
+            all_hists['bkg'][proc] = hist
+    return all_hists
 
-def multicore():
+def CombineCommonSets(groupname):
+    '''Which stitch together either QCD or ttbar (ttbar-allhad+ttbar-semilep)
+
+    @param groupname (str, optional): "QCD" or "ttbar".
+    '''
+    if groupname not in ["QCD","ttbar"]: raise ValueError('Can only combine QCD or ttbar')
+    config = OpenJSON('THconfig.json')
+    for y in ['16','17','18']:
+        outfile = ROOT.TFile.Open('rootfiles/THselection_QCD_%s.root'%y,'RECREATE')
+        lumi = config['lumi'+y]
+        xsecs = {setname:xsec for (setname,xsec) in config['XSECS'].items() if 'QCD' in setname}
+        tfiles = {setname:ROOT.TFile.Open('rootfiles/THselection_%s_%s.root'%(setname,y)) for setname in xsecs.keys()}
+        histnames = [k.GetName() for k in list(tfiles.values())[0].GetListOfKeys()]
+        hists = {setname:{histname:tfile.Get(histname) for histname in histnames} for (setname,tfile) in tfiles.items()}
+        outhists = StitchQCD(hists,{setname:(xsec*lumi) for (setname,xsec) in xsecs.items()})
+        outfile.cd()
+        outhists.Do('Write')
+        outfile.Close()
+    MakeRun2(groupname)    
+
+def MakeRun2(setname):
+    ExecuteCmd('hadd -f rootfiles/THselection_{0}_Run2.root rootfiles/THselection_{0}_16.root rootfiles/THselection_{0}_17.root rootfiles/THselection_{0}_18.root'.format(setname))
+
+def multicore(doJME=True):
+    CompileCpp('THmodules.cc')
     files = GetAllFiles()
-    args = Namespace()
-    pool = multiprocessing.Pool(processes=4)
+    
+    pool = multiprocessing.Pool(processes=8 if doJME else 4)
+    nthreads = 2 if doJME else 4
     process_args = []
     for f in files:
-        args.proc, args.year = GetProcYear(f)
-        args.threads = 4
-        process_args.append(args)
-        if 'Data' not in args.proc and 'QCD' not in args.proc:
-            for jme in ['JES','JER','JMS','JMR']:
-                for v in ['up','down']:
-                    process_args.append(args + ' -v %s_%s'%(jme,v))
+        setname, era = GetProcYearFromTxt(f)
+        
+        if 'Data' not in setname and 'QCD' not in setname:
+            process_args.append(Namespace(threads=nthreads,setname=setname,era=era,variation=''))
+            if doJME:
+                for jme in ['JES','JER','JMS','JMR']:
+                    for v in ['up','down']:
+                        process_args.append(Namespace(threads=nthreads,setname=setname,era=era,variation='%s_%s'%(jme,v)))
+        else:
+            process_args.append(Namespace(threads=nthreads,setname=setname,era=era,variation=''))
+    # for p in process_args:
+    #     print (p)
     pool.map(main,process_args)
 
-def plot():
-    hists = DictStructureCopy(GetAllFiles())
+def plot(histname,fancyname):
+    files = [f for f in glob('rootfiles/THselection_*_16.root') if (('_QCD_' in f) and ('_Data_' in f) and ('_ttbar_' in f) and ('_TprimeB-' in f))]
+    hists = GetHistDict(histname,files)
 
+    CompareShapes('plots/test_2016.pdf','16',fancyname,
+                   bkgs=hists['bkg'],
+                   signals=hists['sig'],
+                   names={},
+                   colors={'QCD':ROOT.kYellow,'ttbar':ROOT.kRed},
+                   scale=True, stackBkg=False, 
+                   doSoverB=False)
+
+if __name__ == '__main__':
+    # multicore(False)
+    CombineCommonSets('QCD')
+    CombineCommonSets('ttbar')
+    MakeRun2('Data')
+    for m in range(800,1900,100):
+        MakeRun2('TprimeB-%s'%m)
+
+    plot('HT','H_T')
--- THselection.py
+++ THselection.py
@@ -11,6 +11,7 @@ def main(args):
     start = time.time()
     selection = THClass('dijet_nano/%s_%s_snapshot.txt'%(args.setname,args.era),int(args.era),1,1)
     # JME variations
+    doStudies = False
     if args.variation.startswith('JE'):
         selection.a.Define('Dijet_pt_corr','hardware::HadamardProduct(Dijet_pt,Dijet_%s)'%args.variation)
         selection.a.Define('Dijet_msoftdrop_corr','hardware::HadamardProduct(Dijet_msoftdrop,Dijet_%s)'%args.variation)
@@ -18,56 +19,59 @@ def main(args):
         selection.a.Define('Dijet_pt_corr','Dijet_pt')
         selection.a.Define('Dijet_msoftdrop_corr','hardware::HadamardProduct(Dijet_msoftdrop,Dijet_%s)'%args.variation)
     else:
+        doStudies = True
         selection.a.Define('Dijet_pt_corr','Dijet_pt')
         selection.a.Define('Dijet_msoftdrop_corr','Dijet_msoftdrop')
 
-    selection.a.Define('pt0','Dijet_pt_corr[0]')
-    selection.a.Define('pt1','Dijet_pt_corr[1]')
-    selection.a.Define('HT','pt0+pt1')
-    selection.a.Define('deltaEta','abs(Dijet_eta[0] - Dijet_eta[1])')
     selection.a.Define('Dijet_vect','hardware::TLvector(Dijet_pt_corr, Dijet_eta, Dijet_phi, Dijet_msoftdrop_corr)')
-    selection.a.Define('deltaY','abs(Dijet_vect[0].Rapidity() - Dijet_vect[1].Rapidity())')
     kinOnly = selection.ApplyStandardCorrections(snapshot=False)
 
-    # Kinematic plots
-    kinPlots = HistGroup('kinPlots')
-    kinPlots.Add('pt0',selection.a.DataFrame.Histo1D(('pt0','Lead jet pt',100,350,2350),'pt0'))
-    kinPlots.Add('pt1',selection.a.DataFrame.Histo1D(('pt1','Sublead jet pt',100,350,2350),'pt1'))
-    kinPlots.Add('HT',selection.a.DataFrame.Histo1D(('HT','Sum of pt of two leading jets',150,700,3700),'HT'))
-    kinPlots.Add('deltaEta',selection.a.DataFrame.Histo1D(('deltaEta','| #Delta #eta |',48,0,4.8),'deltaEta'))
-    kinPlots.Add('deltaY',selection.a.DataFrame.Histo1D(('deltaY','| #Delta y |',60,0,3),'deltaY'))
+    if doStudies:
+        selection.a.Define('pt0','Dijet_pt_corr[0]')
+        selection.a.Define('pt1','Dijet_pt_corr[1]')
+        selection.a.Define('HT','pt0+pt1')
+        selection.a.Define('deltaEta','abs(Dijet_eta[0] - Dijet_eta[1])')
+        
+        kinOnly = selection.a.Define('deltaY','abs(Dijet_vect[0].Rapidity() - Dijet_vect[1].Rapidity())')
+        # Kinematic plots
+        kinPlots = HistGroup('kinPlots')
+        kinPlots.Add('pt0',selection.a.DataFrame.Histo1D(('pt0','Lead jet pt',100,350,2350),'pt0'))
+        kinPlots.Add('pt1',selection.a.DataFrame.Histo1D(('pt1','Sublead jet pt',100,350,2350),'pt1'))
+        kinPlots.Add('HT',selection.a.DataFrame.Histo1D(('HT','Sum of pt of two leading jets',150,700,3700),'HT'))
+        kinPlots.Add('deltaEta',selection.a.DataFrame.Histo1D(('deltaEta','| #Delta #eta |',48,0,4.8),'deltaEta'))
+        kinPlots.Add('deltaY',selection.a.DataFrame.Histo1D(('deltaY','| #Delta y |',60,0,3),'deltaY'))
 
-    # Taggers after mass selection
-    selection.a.Define('TopMassBools','Dijet_msoftdrop_corr > 105 && Dijet_msoftdrop_corr < 210')
-    selection.a.Define('DAK8TopScoresInMassWindow', 'Dijet_deepTagMD_TvsQCD[TopMassBools]')
-    selection.a.Define('PNTopScoresInMassWindow', 'Dijet_particleNet_TvsQCD[TopMassBools]')
-    kinPlots.Add('DAK8TopScoresInMassWindow',selection.a.DataFrame.Histo1D(('DAK8TopScoresInMassWindow','DeepAK8 top score for jets in top mass window',50,0,1),'DAK8TopScoresInMassWindow'))
-    kinPlots.Add('PNTopScoresInMassWindow',selection.a.DataFrame.Histo1D(('PNTopScoresInMassWindow','ParticleNet top score for jets in top mass window',50,0,1),'PNTopScoresInMassWindow'))
+        # Taggers after mass selection
+        selection.a.Define('TopMassBools','Dijet_msoftdrop_corr > 105 && Dijet_msoftdrop_corr < 210')
+        selection.a.Define('DAK8TopScoresInMassWindow', 'Dijet_deepTagMD_TvsQCD[TopMassBools]')
+        selection.a.Define('PNTopScoresInMassWindow', 'Dijet_particleNet_TvsQCD[TopMassBools]')
+        kinPlots.Add('DAK8TopScoresInMassWindow',selection.a.DataFrame.Histo1D(('DAK8TopScoresInMassWindow','DeepAK8 top score for jets in top mass window',50,0,1),'DAK8TopScoresInMassWindow'))
+        kinPlots.Add('PNTopScoresInMassWindow',selection.a.DataFrame.Histo1D(('PNTopScoresInMassWindow','ParticleNet top score for jets in top mass window',50,0,1),'PNTopScoresInMassWindow'))
 
-    selection.a.Define('HiggsMassBools','Dijet_msoftdrop_corr > 100 && Dijet_msoftdrop_corr < 140')
-    selection.a.Define('DAK8HiggsScoresInMassWindow','Dijet_deepTagMD_HbbvsQCD[HiggsMassBools]')
-    selection.a.Define('PNHiggsScoresInMassWindow','Dijet_particleNet_HbbvsQCD[HiggsMassBools]')
-    kinPlots.Add('DAK8HiggsScoresInMassWindow',selection.a.DataFrame.Histo1D(('DAK8HiggsScoresInMassWindow','DeepAK8 Higgs score for jets in Higgs mass window',50,0,1),'DAK8HiggsScoresInMassWindow'))
-    kinPlots.Add('PNHiggsScoresInMassWindow',selection.a.DataFrame.Histo1D(('PNHiggsScoresInMassWindow','ParticleNet Higgs score for jets in Higgs mass window',50,0,1),'PNHiggsScoresInMassWindow'))
+        selection.a.Define('HiggsMassBools','Dijet_msoftdrop_corr > 100 && Dijet_msoftdrop_corr < 140')
+        selection.a.Define('DAK8HiggsScoresInMassWindow','Dijet_deepTagMD_HbbvsQCD[HiggsMassBools]')
+        selection.a.Define('PNHiggsScoresInMassWindow','Dijet_particleNet_HbbvsQCD[HiggsMassBools]')
+        kinPlots.Add('DAK8HiggsScoresInMassWindow',selection.a.DataFrame.Histo1D(('DAK8HiggsScoresInMassWindow','DeepAK8 Higgs score for jets in Higgs mass window',50,0,1),'DAK8HiggsScoresInMassWindow'))
+        kinPlots.Add('PNHiggsScoresInMassWindow',selection.a.DataFrame.Histo1D(('PNHiggsScoresInMassWindow','ParticleNet Higgs score for jets in Higgs mass window',50,0,1),'PNHiggsScoresInMassWindow'))
 
-    # Mass after tagger selection
-    selection.a.Define('TopDAK8Bools','Dijet_deepTagMD_TvsQCD > 0.6')
-    selection.a.Define('TopPNBools','Dijet_particleNet_TvsQCD > 0.6')
+        # Mass after tagger selection
+        selection.a.Define('TopDAK8Bools','Dijet_deepTagMD_TvsQCD > 0.6')
+        selection.a.Define('TopPNBools','Dijet_particleNet_TvsQCD > 0.6')
 
-    selection.a.Define('TopMassAfterDAK8Tag', 'Dijet_msoftdrop_corr[TopDAK8Bools]')
-    selection.a.Define('TopMassAfterPNTag', 'Dijet_msoftdrop_corr[TopPNBools]')
-    kinPlots.Add('TopMassAfterDAK8Tag',selection.a.DataFrame.Histo1D(('TopMassAfterDAK8Tag','Jet mass after DAK8 top score > 0.6',25,50,300),'TopMassAfterDAK8Tag'))
-    kinPlots.Add('TopMassAfterPNTag',selection.a.DataFrame.Histo1D(('TopMassAfterPNTag','Jet mass after PN top score > 0.6',25,50,300),'TopMassAfterPNTag'))
+        selection.a.Define('TopMassAfterDAK8Tag', 'Dijet_msoftdrop_corr[TopDAK8Bools]')
+        selection.a.Define('TopMassAfterPNTag', 'Dijet_msoftdrop_corr[TopPNBools]')
+        kinPlots.Add('TopMassAfterDAK8Tag',selection.a.DataFrame.Histo1D(('TopMassAfterDAK8Tag','Jet mass after DAK8 top score > 0.6',25,50,300),'TopMassAfterDAK8Tag'))
+        kinPlots.Add('TopMassAfterPNTag',selection.a.DataFrame.Histo1D(('TopMassAfterPNTag','Jet mass after PN top score > 0.6',25,50,300),'TopMassAfterPNTag'))
 
-    selection.a.Define('HiggsDAK8Bools','Dijet_deepTagMD_HbbvsQCD > 0.6')
-    selection.a.Define('HiggsPNBools','Dijet_particleNet_HbbvsQCD > 0.6')
+        selection.a.Define('HiggsDAK8Bools','Dijet_deepTagMD_HbbvsQCD > 0.6')
+        selection.a.Define('HiggsPNBools','Dijet_particleNet_HbbvsQCD > 0.6')
 
-    selection.a.Define('HiggsMassAfterDAK8Tag', 'Dijet_msoftdrop_corr[HiggsDAK8Bools]')
-    selection.a.Define('HiggsMassAfterPNTag', 'Dijet_msoftdrop_corr[HiggsPNBools]')
-    kinPlots.Add('HiggsMassAfterDAK8Tag',selection.a.DataFrame.Histo1D(('HiggsMassAfterDAK8Tag','Jet mass after DAK8 Higgs score > 0.6',25,50,300),'HiggsMassAfterDAK8Tag'))
-    kinPlots.Add('HiggsMassAfterPNTag',selection.a.DataFrame.Histo1D(('HiggsMassAfterPNTag','Jet mass after PN Higgs score > 0.6',25,50,300),'HiggsMassAfterPNTag'))
+        selection.a.Define('HiggsMassAfterDAK8Tag', 'Dijet_msoftdrop_corr[HiggsDAK8Bools]')
+        selection.a.Define('HiggsMassAfterPNTag', 'Dijet_msoftdrop_corr[HiggsPNBools]')
+        kinPlots.Add('HiggsMassAfterDAK8Tag',selection.a.DataFrame.Histo1D(('HiggsMassAfterDAK8Tag','Jet mass after DAK8 Higgs score > 0.6',25,50,300),'HiggsMassAfterDAK8Tag'))
+        kinPlots.Add('HiggsMassAfterPNTag',selection.a.DataFrame.Histo1D(('HiggsMassAfterPNTag','Jet mass after PN Higgs score > 0.6',25,50,300),'HiggsMassAfterPNTag'))
 
-    out = ROOT.TFile.Open('rootfiles/THselection_%s_%s.root'%(args.setname,args.era),'RECREATE')
+    out = ROOT.TFile.Open('rootfiles/THselection_%s_%s%s.root'%(args.setname,args.era,'_'+args.variation if args.variation != '' else ''),'RECREATE')
     out.cd()
     for t in ['deepTagMD','particleNet']:
         selection.a.SetActiveNode(kinOnly)
@@ -93,8 +97,9 @@ def main(args):
                 templates = selection.a.MakeTemplateHistos(ROOT.TH2F('MthvMh_%s'%mod_name,'MthvMh %s with %s'%(mod_title,t),40,60,260,28,800,2200),['Higgs_msoftdrop','mth'])
                 templates.Do('Write')
 
-    kinPlots.Do('Write')
-    selection.a.PrintNodeTree('NodeTree.pdf')
+    if doStudies: 
+        kinPlots.Do('Write')
+        selection.a.PrintNodeTree('NodeTree.pdf')
     print ('%s sec'%(time.time()-start))
 
 if __name__ == '__main__':
@@ -110,5 +115,6 @@ if __name__ == '__main__':
                         action='store', default='',
                         help='JES_up, JES_down, JMR_up,...')
     args = parser.parse_args()
+    args.threads = 1
     CompileCpp('THmodules.cc')
     main(args)
