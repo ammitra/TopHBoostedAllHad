91731ccfb2086a5a951e26cc8c6f03b5f20ac3e5
Lucas Corcodilos <lac352@scarletmail.rutgers.edu>
2021-04-30 12:31:33 -0400

    Fix TptReweight, Pileup in THselection step

--- THClass.py
+++ THClass.py
@@ -126,13 +126,13 @@ class THClass:
         
         else:
             if not self.a.isData:
-                self.a.AddCorrection(Correction('pileup',corrtype='weight'))
+                self.a.AddCorrection(Correction('Pileup',corrtype='weight'))
                 self.a.AddCorrection(Correction('Pdfweight',corrtype='uncert'))
                 if self.year == 16 or self.year == 17:
                     self.a.AddCorrection(Correction('Prefire',corrtype='weight'))
                 elif self.year == 18:
                     self.a.AddCorrection(Correction('HEM_drop',corrtype='corr'))
-                if 'ttbar' in self.a.fileName:
+                if 'ttbar' in self.setname:
                     self.a.AddCorrection(Correction('TptReweight',corrtype='weight'))
                 
         return self.a.GetActiveNode()
