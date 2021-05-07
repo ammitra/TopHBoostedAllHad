b5808c2799c48675f2e4ee7d8a274431cb8f8072
Lucas Corcodilos <lac352@scarletmail.rutgers.edu>
2021-05-06 20:08:02 -0400

    Factorize studies to their own script

--- THselection.py
+++ THselection.py
@@ -6,62 +6,18 @@ ROOT.gROOT.SetBatch(True)
 
 from THClass import THClass
 
-def main(args):
+def THselection(args):
     ROOT.ROOT.EnableImplicitMT(args.threads)
     start = time.time()
-    selection = THClass('dijet_nano/%s_%s_snapshot.txt'%(args.setname,args.era),int(args.era))
-    selection.ApplyTrigs()
-    # JME variations
-    doStudies = selection.OpenForSelection(args.variation)
+    selection = THClass('dijet_nano/%s_%s_snapshot.txt'%(args.setname,args.era),int(args.era),1,1)
+    selection.OpenForSelection(args.variation)
     kinOnly = selection.a.MakeWeightCols(extraNominal='' if selection.a.isData else 'genWeight*%s'%selection.GetXsecScale())
-
-    if doStudies:
-        selection.a.Define('pt0','Dijet_pt_corr[0]')
-        selection.a.Define('pt1','Dijet_pt_corr[1]')
-        selection.a.Define('HT','pt0+pt1')
-        selection.a.Define('deltaEta','abs(Dijet_eta[0] - Dijet_eta[1])')
-        
-        kinOnly = selection.a.Define('deltaY','abs(Dijet_vect[0].Rapidity() - Dijet_vect[1].Rapidity())')
-        # Kinematic plots
-        kinPlots = HistGroup('kinPlots')
-        kinPlots.Add('pt0',selection.a.DataFrame.Histo1D(('pt0','Lead jet pt',100,350,2350),'pt0','weight__nominal'))
-        kinPlots.Add('pt1',selection.a.DataFrame.Histo1D(('pt1','Sublead jet pt',100,350,2350),'pt1','weight__nominal'))
-        kinPlots.Add('HT',selection.a.DataFrame.Histo1D(('HT','Sum of pt of two leading jets',150,700,3700),'HT','weight__nominal'))
-        kinPlots.Add('deltaEta',selection.a.DataFrame.Histo1D(('deltaEta','| #Delta #eta |',48,0,4.8),'deltaEta','weight__nominal'))
-        kinPlots.Add('deltaY',selection.a.DataFrame.Histo1D(('deltaY','| #Delta y |',60,0,3),'deltaY','weight__nominal'))
-
-    if not selection.a.isData and doStudies:
-        selection.ApplyTopPickViaMatch()
-        kinPlots.Add('tIdx',selection.a.DataFrame.Histo1D(('tIdx','Top jet idx',2,0,2),'tIdx'))
-        kinPlots.Add('hIdx',selection.a.DataFrame.Histo1D(('hIdx','Higgs jet idx',2,0,2),'hIdx'))
-        
-        selection.a.SetActiveNode(kinOnly)
-        selection.a.ObjectFromCollection('LeadTop','Dijet',0)
-        nminus1Node = selection.a.ObjectFromCollection('SubleadHiggs','Dijet',1)
 
     out = ROOT.TFile.Open('rootfiles/THselection_%s_%s%s.root'%(args.setname,args.era,'_'+args.variation if args.variation != 'None' else ''),'RECREATE')
     out.cd()
     for t in ['deepTag','particleNet']:
         top_tagger = '%s_TvsQCD'%t
         higgs_tagger = '%sMD_HbbvsQCD'%t
-
-        # N-1
-        if not selection.a.isData and doStudies:
-            selection.a.SetActiveNode(nminus1Node)
-            nminusGroup = selection.GetNminus1Group(t)
-            nminusNodes = selection.a.Nminus1(nminusGroup)
-            for n in nminusNodes.keys():
-                if n.startswith('m'):
-                    bins = [25,50,300]
-                    if n.startswith('mH'): var = 'SubleadHiggs_msoftdrop_corr'
-                    else: var = 'LeadTop_msoftdrop_corr'
-                elif n == 'full': continue
-                else:
-                    bins = [20,0,1]
-                    if n.endswith('H_cut'): var = 'SubleadHiggs_%s'%higgs_tagger
-                    else: var = 'LeadTop_%s'%top_tagger
-                print ('N-1: Plotting %s for node %s'%(var,n))
-                kinPlots.Add(n+'_nminus1',nminusNodes[n].DataFrame.Histo1D((n+'_nminus1',n+'_nminus1',bins[0],bins[1],bins[2]),var,'weight__nominal'))
 
         # Signal region
         selection.a.SetActiveNode(kinOnly)
