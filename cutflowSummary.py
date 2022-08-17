2440d8c67987e5e5c012d132cd2a0f4179ad259a
Amitav Mitra <amitra3@jhu.edu>
2022-08-17 10:23:16 -0500

    split up snapshot/selection yields

--- cutflowSummary.py
+++ cutflowSummary.py
@@ -12,13 +12,20 @@ from argparse import ArgumentParser
 from collections import OrderedDict
 import numpy as np
 
-def generateCutflow(setName):
+def generateCutflow(setName, selection=False):
     '''
-	main function to generate the cutflow from both snapshot and selection for a set
+	main function to generate the cutflow from snapshot and/or selection for a set
+	setName   [str]  = set name as seen in snapshot/selection files
+	selection [bool] = False if only snapshot, True if both
     '''
-    # first do snapshots
-    snapVars = OrderedDict([('NPROC',0.),('NFLAGS',0.),('NJETS',0.),('NJETID',0),('NPT',0.),('NKIN',0.),('nTop_CR',0.),('higgsF_CR',0.),('higgsL_CR',0.),('higgsP_CR',0.),('nTop_SR',0.),('higgsF_SR',0.),('higgsL_SR',0.),('higgsP_SR',0.)])
+    if selection:
+	snapVars = OrderedDict([('NPROC',0.),('NFLAGS',0.),('NJETS',0.),('NJETID',0),('NPT',0.),('NKIN',0.),('nTop_CR',0.),('higgsF_CR',0.),('higgsL_CR',0.),('higgsP_CR',0.),('nTop_SR',0.),('higgsF_SR',0.),('higgsL_SR',0.),('higgsP_SR',0.)])
+    else:
+	snapVars = OrderedDict([('NPROC',0.),('NFLAGS',0.),('NJETS',0.),('NJETID',0),('NPT',0.),('NKIN',0.)])
+
     years = OrderedDict([('16',snapVars.copy()), ('16APV', snapVars.copy()), ('17', snapVars.copy()), ('18', snapVars.copy())])
+
+    # do snapshots
     for year, varDict in years.items():
 	#if not os.path.exists('dijet_nano/{}_{}_snapshot.txt'.format(setName,year)):
 	    #pass
@@ -28,48 +35,52 @@ def generateCutflow(setName):
 	    fList = open(snapFile)
 	    rFiles = [i.strip() for i in fList if i != '']
 	    for fName in rFiles:
-	        #print('opening {}'.format(fName))
+	        print('opening {}'.format(fName))
 	        f = ROOT.TFile.Open(fName, 'READ')
 	        # check for empty TTrees
 	        if not f.Get('Events'):
-		    #print('Skipping file due to no Events TTree:\n\t{}'.format(fName))
+		    print('Skipping file due to no Events TTree:\n\t{}'.format(fName))
 		    continue
 	        e = f.Get('Events')
 	        if not e.GetEntry():
-		    #print('Skipping file due to empty Events TTree:\n\t{}'.format(fName))
+		    print('Skipping file due to empty Events TTree:\n\t{}'.format(fName))
 		    continue
 		e.GetEntry()	# initialize all branches
 	        for cut, num in varDict.items():
 		    if ('SR' in cut) or ('CR' in cut):
 		        pass		    
 		    else:
+			print('Getting Leaf {}'.format(cut))
 	                varDict[cut] += e.GetLeaf(cut).GetValue(0)
 	        f.Close()
 
-    # selection will have common sets joined by default (see rootfiles/get_all.py and perform_selection.py)
-    # i.e.: 	THselection_ZJets_18.root
-    for year, varDict in years.items():
-	if not os.path.exists('rootfiles/THselection_{}_{}.root'.format(setName, year)):
-	    pass
-	f = ROOT.TFile.Open('rootfiles/THselection_{}_{}.root'.format(setName, year))
-	h = f.Get('cutflow')
-	for i in range(1, 9):	# cutflow has 8 bins w the cuts, starting from index 1
-	    var = h.GetXaxis().GetBinLabel(i)
-	    val = h.GetBinContent(i)
-	    varDict[var] += val
-	f.Close()
+    if selection:	# do snapshots too
+        # selection will have common sets joined by default (see rootfiles/get_all.py and perform_selection.py)
+        # i.e.: 	THselection_ZJets_18.root
+        for year, varDict in years.items():
+	    if not os.path.exists('rootfiles/THselection_{}_{}.root'.format(setName, year)):
+	        pass
+	    f = ROOT.TFile.Open('rootfiles/THselection_{}_{}.root'.format(setName, year))
+	    h = f.Get('cutflow')
+	    for i in range(1, 9):	# cutflow has 8 bins w the cuts, starting from index 1
+	        var = h.GetXaxis().GetBinLabel(i)
+	        val = h.GetBinContent(i)
+	        varDict[var] += val
+	    f.Close()
 
     # returns dict of {year: cutflowDict}
     return years
 
 
-def printYields():
+def printYields(selection=False):
     '''prints the yields for bkg and data in Latex form'''
-    labels = ["Sample","nProc","nFlags","nJets","nJetID","p_{t}","nKin","CR_topCut","CR_F","CR_L","CR_P","SR_topCut","SR_F","SR_L","SR_P"]
+    labels = ["Sample","nProc","nFlags","nJets","nJetID","p_{t}","nKin"]
+    if selection:
+	labels.extend(["CR_topCut","CR_F","CR_L","CR_P","SR_topCut","SR_F","SR_L","SR_P"])
     sets = ["ttbar", "QCD", "WJets", "ZJets", "Data"]
 
     for s in sets:
-        res = generateCutflow(s)
+        res = generateCutflow(s, selection)
         print('\n------------------ {} -------------------'.format(s))
         for year, dicts in res.items():
 	    print('----------------- {} -----------------'.format('20'+year))
@@ -82,19 +93,21 @@ def printYields():
             print(" & ".join(labels)+" \\\\")
             print(latexRow)
 
-def printEfficiencies():
+def printEfficiencies(selection=False):
     '''prints the efficiencies for various T' samples.
 	First, for mT = 1800, mPhi = 75, 100, 125, 250, 500
 	Then, for mPhi = 125, mT = 1400, 1500, 1600, 1700, 1800  (SKIP 1700 - it's missing 2018)
     '''
-    labels = ["Sample","nProc","nFlags","nJets","nJetID","p_{t}","nKin","CR_topCut","CR_F","CR_L","CR_P","SR_topCut","SR_F","SR_L","SR_P"]
+    labels = ["Sample","nProc","nFlags","nJets","nJetID","p_{t}","nKin"]
+    if selection:
+	labels.extend(["CR_topCut","CR_F","CR_L","CR_P","SR_topCut","SR_F","SR_L","SR_P"])
     # first keep constant mT
     prefix = 'TprimeB'
     mT = 1800
     phiMasses = [75, 100, 125, 250, 500]
     for mPhi in phiMasses:
 	nProc = 0
-	res = generateCutflow('{}-{}-{}'.format(prefix, mT, mPhi))
+	res = generateCutflow('{}-{}-{}'.format(prefix, mT, mPhi), selection)
 	print('\n------------------ {} -------------------'.format('{}-{}-{}'.format(prefix, mT, mPhi)))
 	for year, dicts in res.items():
 	    print('----------------- {} -----------------'.format('20'+year))
@@ -127,6 +140,13 @@ def printEfficiencies():
             print(" & ".join(labels)+" \\\\")
             print(latexRow)
 
-printEfficiencies()
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument('--selection', action='store_true',
+			help='Get cutflow/yield info for selection too. Do not include if you only want snapshot info.')
+    
+    args = parser.parse_args()
 
+    printYields(args.selection)
 
