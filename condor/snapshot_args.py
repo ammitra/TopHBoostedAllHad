4c4d5f8489d829b69fabcf29c81a891c691454f4
Amitav Mitra <amitra3@jhu.edu>
2022-08-19 16:48:26 -0500

    fix integer division bug which skipped some args being written

--- condor/snapshot_args.py
+++ condor/snapshot_args.py
@@ -1,25 +1,21 @@
 import glob,os
 
 out = open('condor/snapshot_args.txt','w')
+
 for f in glob.glob('raw_nano/*.txt'):
     if os.path.getsize(f) == 0:
         print ('File %s is empty... Skipping.'%(f))
         continue
+
     filename = f.split('/')[-1].split('.')[0]
     nfiles = len(open(f,'r').readlines())
     setname = filename.split('_')[0]
     year = filename.split('_')[1]
 
     njobs = int(nfiles/2)
+    if njobs == 0:	# this occurs when nfiles = 1
+	njobs += 1
     for i in range(1,njobs+1):
 	out.write('-s %s -y %s -j %s -n %s \n'%(setname,year,i,njobs))
-    '''
-    if 'Tprime' in setname:
-        out.write('-s %s -y %s\n'%(setname,year))
-    else:
-        njobs = int(nfiles/2)
-        for i in range(1,njobs+1):
-            out.write('-s %s -y %s -j %s -n %s \n'%(setname,year,i,njobs))
-    '''
 
 out.close()
