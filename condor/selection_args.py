c7cb6f615eb7ee19caa6ddde17488af5fb36abcc
Amitav Mitra <amitra3@jhu.edu>
2023-02-02 13:34:16 -0600

    update selection to use HT-specific trigger efficiencies

--- condor/selection_args.py
+++ condor/selection_args.py
@@ -1,22 +1,6 @@
 from glob import glob
 import os
 
-'''
-out = open('condor/selection_args.txt','w')
-for f in glob('dijet_nano/*.txt'):
-    if os.path.getsize(f) == 0:
-	print('File {} is empty, skipping...'.format(f))
-	continue
-    filename = f.split('/')[-1].split('.')[0]
-    nfiles = len(open(f,'r').readlines())
-    setname = filename.split('_')[0]
-    year = filename.split('_')[1]
-
-
-    njobs = int(nfiles/2)
-    for i in range(1,njobs+1):
-        out.write('-s %s -y %s -j %s -n %s \n'%(setname,year,i,njobs))
-'''
 def GetAllFiles():
     return [f for f in glob('dijet_nano/*_snapshot.txt') if f != '']
 def GetProcYearFromFile(filename):
@@ -26,19 +10,25 @@ def GetProcYearFromFile(filename):
     else:
 	return pieces[1], pieces[2]
 
-out = open('condor/selection_args.txt','w')
-files = GetAllFiles()
-for f in files:
-    setname, era = GetProcYearFromFile(f)
-    if 'Data' not in setname and 'QCD' not in setname:
-	out.write('-s {} -y {} -v None \n'.format(setname, era))  # perform nominal variation first
-	JME = ['JES', 'JER', 'JMS', 'JMR']	# normal Jet corrections
-	if 'Tprime' in setname:
-	    JME.extend(['PNetTop', 'PNetXbb'])	# particleNet SF corrections (See THselection.py, need to be named PNet_up/down
-	for jme in JME:
-	    for v in ['up', 'down']:
-		out.write('-s {} -y {} -v {}_{} \n'.format(setname, era, jme, v))
-    else: 
-	out.write('-s {} -y {} -v None \n'.format(setname, era))
-
-out.close()
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument('--HT', type=str, dest='HT',
+                        action='store', default='0',
+                         help='Value of HT to cut on')
+    args = parser.parse_args()
+    out = open('condor/selection_args_HT{}.txt'.format(args.HT),'w')
+    files = GetAllFiles()
+    for f in files:
+	setname, era = GetProcYearFromFile(f)
+   	if 'Data' not in setname and 'QCD' not in setname:
+	    out.write('-s {} -y {} -v None --HT {}\n'.format(setname, era, args.HT))  # perform nominal variation first
+	    JME = ['JES', 'JER', 'JMS', 'JMR']	# normal Jet corrections
+	    if 'Tprime' in setname:
+		JME.extend(['PNetTop', 'PNetXbb'])	# particleNet SF corrections (See THselection.py, need to be named PNet_up/down
+	    for jme in JME:
+	    	for v in ['up', 'down']:
+		    out.write('-s {} -y {} -v {}_{} --HT {}\n'.format(setname, era, jme, v, args.HT))
+    	else: 
+	    out.write('-s {} -y {} -v None --HT {}\n'.format(setname, era, args.HT))
+    out.close()
