fd3efb4ec1ebdd5de54eb209f3212105589cf7e7
Amitav Mitra <amitra3@jhu.edu>
2024-02-17 15:18:07 -0600

    revert

--- THClass.py
+++ THClass.py
@@ -405,29 +405,19 @@ class THClass:
 	else:
 	    WP = 0.685 if loose else 0.92
 	checkpoint = self.a.GetActiveNode()
-	passFail = {'SRloose':None,'fail':None,'pass':None,'fail_notorthog':None,'pass_notorthog':None}
+	passFail = {'SRloose':None,'fail':None,'pass':None}
 
-	# NOT ORTHOGONAL TO SR
+	# Start out with SR loose definition (since we don't use Fail due to high statistics). 
+	# The SR loose will be the starting point for both the SR and ttbarCR in the joint fit:
+	# 	SR_loose -> SR_pass      &&      SR_loose -> ttbarCR_pass
 	self.a.SetActiveNode(checkpoint)
 	passFail['SRloose'] = self.a.Cut('ttbarCR_Hbb_loose','Higgs_{0} > 0.8 && Higgs_{0} < {1}'.format(tagger,0.98) if not signal else 'NewTagCats==1')
-	# The Fail region is then a failing deepAK8MD tagger
+	# The Fail region is then a failing deepAK8MD tagger (won't be used in fit, just for reference)
 	self.a.SetActiveNode(checkpoint)
-	passFail['fail_notorthog'] = self.a.Cut('ttbarCR_notorthog_fail','Higgs_{0} < {1}'.format(topTagger,WP))
+	passFail['fail'] = self.a.Cut('ttbarCR_top_fail','Higgs_{0} < {1}'.format(topTagger,WP))
 	# the Pass region is then a deepAK8 MD top tagger > some working point
 	self.a.SetActiveNode(checkpoint)
-	passFail['pass_notorthog'] = self.a.Cut('ttbarCR_notorthog_pass','Higgs_{0} > {1}'.format(topTagger,WP))
-
-	# ORTHOGONAL TO SR
-	self.a.SetActiveNode(checkpoint)
-	# In order to ensure orthogonality between the ttbarCR and the SR, construct the ttbarCR from the SR Fail cut (Xbb < 0.8)
-	self.a.Cut('ttbarCR_base_cut','Higgs_{0} < 0.8'.format(tagger))
-	ttCR_checkpoint = self.a.GetActiveNode()
-	# The Fail region is then a deepAK8 MD top tagger < some working point 
-	self.a.SetActiveNode(ttCR_checkpoint)
-	passFail['fail'] = self.a.Cut('ttbarCR_fail','Higgs_{0} < {1}'.format(topTagger,WP))
-	# the Pass region is then a deepAK8 MD top tagger > some working point
-	self.a.SetActiveNode(ttCR_checkpoint)
-	passFail['pass'] = self.a.Cut('ttbarCR_pass','Higgs_{0} > {1}'.format(topTagger,WP))
+	passFail['pass'] = self.a.Cut('ttbarCR_top_pass','Higgs_{0} > {1}'.format(topTagger,WP))
         # reset active node, return dict
         self.a.SetActiveNode(checkpoint)
         return passFail
